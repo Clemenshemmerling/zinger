71cbbc31bfffa42c4d85dc963a4265d1befed3ba
Clemens Hemmerling <clemens@galileo.edu>
2018-08-26 12:07:00 -0600

    Creamos servicio de autenticacion

---
+++ src/services/auth.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireAuth} from "angularfire2/auth";
+
+@Injectable()
+
+export class AuthService {
+    registerWithEmail(email, password) {
+        return this.firebaseAuth.auth.createUserWithEmailAndPassword(email, password);
+    }
+      loginWithEmail(email, password) {
+        return this.firebaseAuth.auth.signInWithEmailAndPassword(email, password);
+    }
+}
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,11 +1,32 @@
 import { Injectable } from "@angular/core";
 import { Status, User } from "../interfaces/user";
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Injectable()
 
 export class UserService {
-    constructor() {
+    constructor(private afDb: AngularFireDatabase) {
         console.log(status);
+    }
+
+    getUsers() {
+        return this.afDb.list('/users/');
+    }
+
+    getUser(uid) {
+        return this.afDb.object('/users/' + uid);
+    }
+
+    createUser(user) {
+        return this.afDb.object('/users/' + user.uid).set(user);
+    }
+
+    editUser(user) {
+        return this.afDb.object('/users/' + user.uid).set(user);
+    }
+
+    deleteUser(user) {
+        return this.afDb.object('/users/' + user.uid).remove();
     }
 
     private users: User[] = [
